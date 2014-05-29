1b655ae8f4908997ddda0db22ea971aab65b9ffd
Noel Sharpe <noels@radnetwork.co.uk>
2014-05-29 16:26:01 +0100

    Typo.

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,13 +6,13 @@
 class tomcat::params {
   case $::osfamily {
     'Debian': {
-      $version      = 6,
-      $java_home    = undef,
-      $java_opts    = '-Djava.awt.headless=true -Xmx128m -XX:+UseConcMarkSweepGC',
-      $jsp_compiler = undef,
-      $security     = undef,
-      $logfile_days = undef,
-      $jvm_tmp      = undef,
+      $version      = 6
+      $java_home    = undef
+      $java_opts    = '-Djava.awt.headless=true -Xmx128m -XX:+UseConcMarkSweepGC'
+      $jsp_compiler = undef
+      $security     = undef
+      $logfile_days = undef
+      $jvm_tmp      = undef
       $authbind     = undef
       $package_name = "tomcat${version}"
       $service_name = "tomcat${version}"
