7addda41daa090591574e9bfc4b740c34af8bcfb
David Otton <david@otton.org>
2014-09-16 15:53:30 +0100

    Install 6 or 7

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,14 +2,25 @@
 #
 # This class is called from tomcat
 #
-class tomcat::config inherits tomcat::params {
+class tomcat::config(
+  $authbind,
+  $http_port,
+  $java_home,
+  $java_opts,
+  $jsp_compiler,
+  $jvm_tmp,
+  $logfile_days,
+  $package_name,
+  $security,
+  $version,
+) {
 
-  file { "/etc/tomcat${tomcat::params::version}/server.xml":
-    content => template( 'tomcat/server.xml' )
+  file { "/etc/${package_name}/server.xml":
+    content => template( "tomcat/server.xml.${version}" )
   }
-
-  file { "/etc/default/tomcat${tomcat::params::version}":
+  file { "/etc/default/${package_name}":
     ensure  => file,
     content => template('tomcat/tomcat_default.erb'),
   }
+
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -3,8 +3,12 @@
 # This class is meant to be called from tomcat
 # It ensure the service is running
 #
-class tomcat::service {
-  service { $tomcat::params::service_name:
+class tomcat::service (
+  $service_name,
+) {
+  include tomcat::params
+
+  service { $service_name:
     ensure     => running,
     enable     => true,
     hasstatus  => true,
