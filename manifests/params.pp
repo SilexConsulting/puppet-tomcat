62d63f451ff445258280298f120abc8812c99ad1
Noel Sharpe <noels@radnetwork.co.uk>
2015-02-03 08:55:06 +0000

    java_opts is meant to be an array.

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,7 +6,7 @@
 class tomcat::params(
   $version      = 7,
   $java_home    = undef,
-  $java_opts    = '-Djava.awt.headless=true -Xmx128m -XX:+UseConcMarkSweepGC',
+  $java_opts    = ['-Djava.awt.headless=true', '-Xmx128m', '-XX:+UseConcMarkSweepGC'],
   $jsp_compiler = undef,
   $security     = undef,
   $logfile_days = undef,
